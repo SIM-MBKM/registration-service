d0664c02983cb881c572290fd2696643a285f961
Dimas Fadilah Akbar <dimasfadilah20@gmail.com>
2025-03-18 16:45:09 +0700

    add registration routes with CRUD operations for managing registrations

---
+++ routes/registration_route.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"registration-service/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegistrationRoutes(router *gin.Engine, programTypeController controller.RegistrationController) {
+	registrationServiceRoute := router.Group("/registration-service/api")
+	{
+		registrationRoutes := registrationServiceRoute.Group("/registration")
+		{
+			registrationRoutes.GET("/", programTypeController.GetAllRegistrations)
+			registrationRoutes.GET("/:id", programTypeController.GetRegistrationByID)
+			registrationRoutes.POST("/", programTypeController.CreateRegistration)
+			registrationRoutes.PUT("/:id", programTypeController.UpdateRegistration)
+			registrationRoutes.DELETE("/:id", programTypeController.DeleteRegistration)
+		}
+	}
+}
