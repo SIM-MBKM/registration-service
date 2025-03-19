0536bfa7ed8a90ce23b5fbce48021c2bd3e9fba7
Dimas Fadilah Akbar <dimasfadilah20@gmail.com>
2025-03-19 15:20:52 +0700

    feat: add document management routes with CRUD operations

---
+++ routes/document_route.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"registration-service/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func DocumentRoutes(router *gin.Engine, documentController controller.DocumentController) {
+	documentServiceRoute := router.Group("/document-service/api")
+	{
+		documentRoutes := documentServiceRoute.Group("/document")
+		{
+			documentRoutes.GET("/", documentController.GetAllDocuments)
+			documentRoutes.GET("/:id", documentController.GetDocumentByID)
+			documentRoutes.POST("/", documentController.CreateDocument)
+			documentRoutes.PUT("/:id", documentController.UpdateDocument)
+			documentRoutes.DELETE("/:id", documentController.DeleteDocument)
+		}
+	}
+}
